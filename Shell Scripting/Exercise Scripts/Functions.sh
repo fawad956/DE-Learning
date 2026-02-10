ec6583c0dfbfed7bdc3c20fe51ca0e66715d0954
fawad956 <34732416+fawad956@users.noreply.github.com>
2026-02-09 20:44:51 -0600

    update functions

--- Shell Scripting/Exercise Scripts/Functions.sh
+++ Shell Scripting/Exercise Scripts/Functions.sh
@@ -27,3 +27,33 @@ echo $GLOBAL_VAR
 my_function
 #GLOBAL_VAR is now available
 echo $GLOBAL_VAR
+
+function backup_file() {
+    if [ -f $1]
+    then
+       BACK="/tmp/$$(basename ${1}).$(date +&F).$$"
+       echo "Backing up $1 tp ${BACK}"
+       cp $1 $BACK
+    fi
+}
+backup_file /etc/hosts
+if [ $? -eq 0]
+then
+    echo "Backup successful"
+fi
+
+**With elif
+function backup_file() {
+    if [ -f $1]
+    then
+        local BACK="/tmp/$(basename ${1}).$(date +&F).SS"
+        echo "Backing up $1 to ${BACK}"
+        #The exit status of the function will be the exit status of the cp command
+        cp $1 $BACK
+    else
+        #The does not exist
+        return 1
+    
+}
+
+
